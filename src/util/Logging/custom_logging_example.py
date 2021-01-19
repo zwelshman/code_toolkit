b5ad603e6afe54de0f4ea091225be8bde78b679a
Zach Welshman <zmswelshman@gmail.com>
2021-01-19 13:38:17 +0000

    custom logging

---
+++ src/util/Logging/custom_logging_example.py
@@ -0,0 +1,40 @@
+import logging
+from os import waitpid
+
+
+DIR = "src\\util\\Logging\\"
+
+CUSTOM_DATA = {
+    "user":"zach@me.com"
+}
+
+def some_function():
+    logging.debug("This is a debugger message", extra=CUSTOM_DATA)
+
+def main():
+    # set custom log level with some parameters
+    
+
+    fmtstr = "User:%(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s "
+    datestr = "%d/%m/%Y %I:%M:%S %p" 
+
+    logging.basicConfig(filename=DIR + "/output.log", 
+                    level=logging.DEBUG, 
+                    filemode='w',
+                    format=fmtstr,
+                    datefmt = datestr)
+    
+    #log start
+    logging.info("Started process", extra=CUSTOM_DATA)
+    
+    #calling func
+    some_function()
+    
+    #log end
+    logging.warning("Ended process", extra=CUSTOM_DATA)
+
+    
+if __name__ == "__main__":
+    
+    main()
+    
--- src/util/Logging/logging_example.py
+++ src/util/Logging/logging_example.py
@@ -1,22 +1,26 @@
-#using logging api
+# using logging api
 
 import logging
 
 
+dir = "src\\util\\Logging\\"
+
 
 def main():
-    #set basic log level
-    logging.basicConfig(level=logging.DEBUG, filename = "output.log")
+    # set basic log level with some parameters
+    logging.basicConfig(level=logging.DEBUG, 
+                    filename=dir + "/output.log",
+                    filemode='w')
 
-    logging.debug('this is a debugging')
+    logging.debug("this is a debugging")
+    logging.info("this is a  info")
+    logging.warning("this is a warning")
+    logging.error("this is a error")
+    logging.critical("this is a critial")
 
-    logging.info('this is a  info')
-
-    logging.warning('this is a warning')
-
-    logging.error('this is a error')
-
-    logging.critical('this is a critial')
+    string = 'string'
+    number = 10
+    logging.info(f'Here is a {string} variable and a {number}')
 
 if __name__ == "__main__":
     main()
