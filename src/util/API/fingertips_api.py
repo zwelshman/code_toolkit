efc39fae2c8de7629283cf35d0c4a911dd892768
Zach Welshman <zmswelshman@gmail.com>
2021-01-20 19:02:28 +0000

    fingertips API with logs

--- src/util/API/fingertips_api.py
+++ src/util/API/fingertips_api.py
@@ -1,6 +1,41 @@
+import logging
 import fingertips_py as ftp 
 
-phof = ftp.get_profile_by_name('public health outcomes framework') 
-phof_meta = ftp.get_metadata_for_profile_as_dataframe(phof['Id']) 
-indicator_meta = phof_meta[phof_meta['Indicator'].str.contains('Healthy')] 
-print(phof_meta)
+DIR = "src\\util\\API\\"
+
+CUSTOM_DATA = {
+    "user":"zach Welshman"
+}
+
+def get_df():
+    df = ftp.get_data_for_indicator_at_all_available_geographies(indicator_id=92603)   
+    logging.debug(f" Making API call from get_df", extra=CUSTOM_DATA)
+    return df
+
+def main():
+
+    fmtstr = "User:%(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s "
+    datestr = "%d/%m/%Y %I:%M:%S %p" 
+
+    logging.basicConfig(filename=DIR + "/phe_api_call.log", 
+                    level=logging.DEBUG, 
+                    filemode='w',
+                    format=fmtstr,
+                    datefmt = datestr)
+    
+    #log start
+    logging.info("Started process....", extra=CUSTOM_DATA)
+    
+    #calling func
+    data = get_df()
+    
+  
+    #log end
+    if len(data) == 0:
+        raise AssertionError (logging.critical(f"Ended process, data retrieved 0 rows", extra=CUSTOM_DATA))
+    else:
+        logging.info(f"Ended process, data retrieved {len(data)} rows", extra=CUSTOM_DATA)
+
+
+if __name__ == "__main__":
+    main()
