afbfaa6799e8beaebfe9ede84ac84158f2572b74
Zach Welshman <38403427+zwelshman@users.noreply.github.com>
2021-06-08 10:02:46 +0100

    Create main.py

---
+++ src/util/docker/main.py
@@ -0,0 +1,11 @@
+def download_data(request):
+  '''example function for gcloud'''
+  
+  try:
+    
+    print('hello')
+    
+  return "Function Finished"
+  
+  except:
+    return "Function did not Finish"
